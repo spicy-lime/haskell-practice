a951b07a23fd990144155aa606799d14eacaa73a
spicy-lime <mjthomps@protonmail.com>
2021-04-06 19:26:29 -0400

    practice

--- baby.hs
+++ baby.hs
@@ -1,21 +1,6 @@
 module Main where
 
-data JsonValue
-  = JsonNull
-  | JsonBool Bool
-  | JsonNumber Integer
-  | JsonString String
-  | JsonArray [JsonValue]
-  | JsonObject [(String, JsonValue)]
-  deriving (Show, Eq)
-
-newtype Parser a = Parser
-  { runParser :: String -> Maybe (String, a)}
-
-jsonNull :: Parser JsonValue
-jsonNull = undefined
-
-
+-- checks if element is in list
 elem' :: (Eq a) => a -> [a] -> Bool
 elem' a as 
   | as == [] = False
@@ -24,12 +9,14 @@ elem' a as
   where
     x = head as
 
+-- removes duplicate elements from list
 nub' :: (Eq a) => [a] -> [a]
 nub' [] = []
 nub' (a:as) 
   | elem' a (as) = nub' (as)
   | True = (a : (nub' as))
-  
+
+-- checks if list in ascending order
 isAsc :: [Int] -> Bool
 isAsc [] =  True
 isAsc [_] = True
@@ -37,9 +24,21 @@ isAsc (x:y:xs)
   | x < y = isAsc (y:xs)
   | True = False
 
+-- directed graph pathfinder
+-- (1,2)(4,5)(2,4)
+hasPath :: [(Int,Int)] -> Int -> Int -> Bool
+hasPath [] _ _ = False
+hasPath ((x,y):xs) a b = undefined
+  
+  
 
-jsonValue :: Parser JsonValue
-jsonValue = undefined
+--hasPath = \ ((:)((,) x y) xs) a b ->
+--  case xs of
+--    x == a && y == b 
+    
+    
+  
+
 
 lucky :: (Integral a) => a -> String
 lucky 7 = "SEVEN"
